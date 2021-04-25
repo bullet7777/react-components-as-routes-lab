4607804a3cd4031effb958b7c4f700c3eb50d513
chris <chris.mahfouz@hotmail.com>
2021-04-25 12:00:58 +0300

    commit tree 09d99287e81828c14f65dc1f16244f4af6c575ea

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
-  );
-}
+     <h1>Directors Page</h1>
+     {directors.map((dir) => {
+       return <div>
+         <h2>Name: {dir.name}</h2>
+         
+         <ul>{dir.movies.map((mov)=>{
+           return<li>{mov}</li>
+         })}</ul>
 
+         </div>
+     })}
+     
+    </div>
+    )
+  }
 export default Directors
