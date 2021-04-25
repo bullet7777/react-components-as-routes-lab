6240efcc69ef8b2b9a310d76732c346a6cd9d32e
chris <chris.mahfouz@hotmail.com>
2021-04-25 12:01:09 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,15 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((act)=>{
+        return <div>
+          <h2>Name: {act.name}</h2>
+          <ul>{act.movies.map((mov)=>{
+return <li>{mov}</li>
+          })}</ul>
+        </div>
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       {movies.map((mov)=>{
+         return <div>
+           <h2>Name: {mov.title}</h2>
+           <h1>Time: {mov.time}</h1>
+         
+
+           <ul>{mov.genres.map((gen)=>{
+             return <li>{gen}</li>
+           })}</ul>
+           </div>
+       })}
     </div>
   );
 };
